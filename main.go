9947fa4cbfa6a874499708bd8652ee07ef174bf1
Caien1 <pulwama01@gmail.com>
2025-04-25 15:59:07 +0530

    run method

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+//dovker run imagen <cmd> <paras>
+// go run main.go run
 
 func main() {
-	fmt.Printf("It is gonna be epic")
+	switch os.Args[1] {
+	case "run":
+		run()
+
+	default:
+		panic("bad command")
+
+	}
+}
+
+func run() {
+	fmt.Print("Running ", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+
+	cmd.Run()
 
 }
